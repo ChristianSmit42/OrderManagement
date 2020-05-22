2ab7fa7d8f9959ec5ed3bbc13fa2a276e5fc8e16
Christian Smit <chrsmit@web.de>
2020-05-22 11:12:55 +0200

    ADD OrderDb ProductDb

--- src/main/java/de/neuefische/ordermanagement/model/Order.java
+++ src/main/java/de/neuefische/ordermanagement/model/Order.java
@@ -1,4 +1,52 @@
 package de.neuefische.ordermanagement.model;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Order {
+    int id;
+    ArrayList<Product> products = new ArrayList<Product>();
+
+    public Order(int id, ArrayList<Product> products) {
+        this.id = id;
+        this.products = products;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", products=" + products +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return id == order.id &&
+                Objects.equals(products, order.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, products);
+    }
 }
